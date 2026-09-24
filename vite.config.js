6435f64c05447c9ef1390a47cac89dbf96b2e9d5
Lighting6647 <157086701+Lighting6647@users.noreply.github.com>
2026-09-24 12:46:32 +0700

    chore: fix chunk size warning

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 700,
+    chunkSizeWarningLimit: 1500,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          chart: ['recharts'],
+          editor: ['react-quill-new']
+        }
+      }
+    }
   },
 })
